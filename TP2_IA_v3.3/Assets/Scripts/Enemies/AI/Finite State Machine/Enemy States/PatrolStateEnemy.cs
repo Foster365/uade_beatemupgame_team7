975f0e01bff96f9ac15eb977f51f022b9f196e84
olimpick11 <toraagu123@gmail.com>
2020-10-31 18:23:33 -0300

    Waypoints, Steering behaviours, decision trees, enemycontroller.
    Movimiento del enemigo con waypoints, steering behaviour seek y flee puesto en el enemycontroller con decision trees, agregado tambien el bool de dead del enemigo, junto con maxhealth y currenthealth para que funcione el flee.

---
+++ TP2_IA_v3.3/Assets/Scripts/Enemies/AI/Finite State Machine/Enemy States/PatrolStateEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolStateEnemy<T>:FSMState<T>
+{
+    Enemy _enemy;
+    EnemyAnimations _enemyAnimations;
+    public PatrolStateEnemy(Enemy enemy, EnemyAnimations enemyAnimations)
+    {
+        _enemy=enemy;
+        _enemyAnimations=enemyAnimations;
+    }
+    
+    public override void Awake()
+    {
+        Debug.Log("Enemy Idle State Awake");
+    }
+
+    public override void Execute()
+    {
+        Debug.Log("Enemy Idle State Execute");
+        _enemy.GoToWaypoint();
+        _enemyAnimations.MoveAnimation(true);
+    }
+
+    public override void Sleep()
+    {
+        Debug.Log("Enemy Idle State Sleep");
+    }
+}
---
+++ TP2_IA_v3.3/Assets/Scripts/Player/Basics/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour, IMove
+{
+    public float speed;
+    public float powerJump;
+    public float maxHealth = 100;
+    private float currentHealth;
+    public bool dead;
+    Rigidbody _rb;
+    //PlayerAnimation _playerAnimation;
+
+    public float CurrentHealth { get => currentHealth; set => currentHealth = value; }
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        CurrentHealth = maxHealth;
+        //_playerAnimation = GetComponent<PlayerAnimation>();
+    }
+    public void Move(Vector3 dir)
+    {
+        dir.y = 0;
+        _rb.velocity = dir * speed;
+        if(dir.x != 0 || dir.z != 0)
+        transform.forward = dir;
+        
+        //Debug.Log(dir);
+        //_playerAnimation.RunAnim(_rb.velocity.magnitude);
+    }
+    public void TakeDamage(float damage)
+    {
+        CurrentHealth -= damage;
+
+        if (CurrentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+    private void Die()
+    {
+        dead = true;
+
+        SceneManager.LoadScene("Level");
+    }
+    public void Jump()
+    {
+        _rb.AddForce(Vector3.up * powerJump, ForceMode.Impulse);
+    }
+}
