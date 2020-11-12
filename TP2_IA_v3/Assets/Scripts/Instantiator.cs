23a9d33fec8da999502fdaaa80f8d711178ba1e4
mpincen <maxipincen09@gmail.com>
2020-11-12 18:55:26 -0300

    Instanciador de enemigos
    lo que dice el titulo :P

---
+++ TP2_IA_v3/Assets/Scripts/Instantiator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiator : MonoBehaviour
+{
+    GameObject _enemys;
+    public float creationTime = 2f, creationRange = 2f;
+    void Start()
+    {
+        _enemys = GameObject.FindGameObjectWithTag("Enemy");
+        InvokeRepeating("Create", 0, creationTime);
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Create()
+    {
+        Vector3 SpawnP = new Vector3(0, 0, 0);
+        SpawnP = transform.position + Random.onUnitSphere * creationRange;
+        SpawnP = new Vector3(SpawnP.x, 2, SpawnP.z);
+
+        GameObject _enemy = Instantiate(_enemys, SpawnP, Quaternion.identity);
+    }
+}
