595eb1ed11dc08577802502a9f9398d3c3297005
olimpick11 <toraagu123@gmail.com>
2020-11-12 22:21:45 -0300

    na

--- TP2_IA_v3/Assets/Scripts/AI/Steering Behaviours/SteeringBehaviour.cs
+++ TP2_IA_v3/Assets/Scripts/AI/Steering Behaviours/SteeringBehaviour.cs
@@ -10,11 +10,6 @@ public abstract class SteeringBehaviour : MonoBehaviour
 
     //Las funciones abstractas, al igual que las interfaces, definen un contrato que las clases que hereden de esta clase tiene que cumplir e implementar
     //la función Move. Por eso no tiene cuerpo en este clase, porque será definido por las clases concretas.
-    protected abstract void Move();
+    protected abstract void Move(Vector3 dir);
 
-    //Virtual permite que las clases que hereden de esta clase tengan la posibilidad de expandir o pisar el comportamiento (con override)
-    protected virtual void Update()
-    {
-        Move();
-    }
 }
