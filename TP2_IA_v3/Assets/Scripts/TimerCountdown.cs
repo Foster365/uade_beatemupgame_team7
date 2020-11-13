4ecc1d1dbc9aecbd4d56283eb40091ec21910cc3
maurojrey <69821300+maurojrey@users.noreply.github.com>
2020-11-13 00:27:28 -0300

    Canvas Timer

---
+++ TP2_IA_v3/Assets/Scripts/TimerCountdown.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerCountdown : MonoBehaviour
+{
+    public GameObject textDisplay;
+    public int secondsLeft = 180;
+    public bool takingAway = false;
+
+    void Start()
+    {
+        textDisplay.GetComponent<Text>().text = "";
+    }
+
+    private void Update()
+    {
+        if(takingAway == false && secondsLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+    }
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+        if (secondsLeft > 59) {
+            textDisplay.GetComponent<Text>().text = "0"+ secondsLeft/60 + ":" + secondsLeft%60;
+        }
+        else if (secondsLeft > 10)
+        {
+            textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+        }
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "00:0" + secondsLeft;
+        }
+        
+        takingAway = false;
+    }
+}
